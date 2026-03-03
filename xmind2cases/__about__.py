7277f80f793336e52125110b03e85e2b17d987ce
koco-co <kopohub@gmail.com>
2026-03-04 01:46:57 +0800

    fix: 恢复 .gitignore 中的 .vscode/ 规则并清理重复关键词
    - 在 .gitignore 中恢复 .vscode/ 忽略规则
    - 清理 xmind2cases/__about__.py 中重复的 "xmind2cases" 关键词

--- xmind2cases/__about__.py
+++ xmind2cases/__about__.py
@@ -1,8 +1,6 @@
 __title__ = "xmind2cases"
 __description__ = "XMind2Cases基于Python实现，提供了一个高效测试用例设计的解决方案！"
-__keywords__ = (
-    "xmind2cases, xmind2cases, testcase, test, testing, xmind, 思维导图, XMind思维导图",
-)
+__keywords__ = ("xmind2cases, testcase, test, testing, xmind, 思维导图, XMind思维导图",)
 __url__ = "https://github.com/zhuifengshen/xmind2cases"
 __author__ = "Devin"
 __author_email__ = "1324556701@qq.com"
